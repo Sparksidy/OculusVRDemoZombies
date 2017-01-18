8880f22f7b5e0214ace9210e6b185fac460c65e8
Siddharth Gupta <sidystan@gmail.com>
2017-01-17 23:38:29 -0800

    First Commit

---
+++ Assets/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        Debug.Log("hello");
+        if (collision.gameObject.name == "Player")
+        {
+            Debug.Log("attack player");
+            Destroy(collision.gameObject);
+        }
+    }
+
+}
---
+++ Assets/FollowTarget.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour {
+
+    public Transform target;
+    NavMeshAgent agent;
+	// Use this for initialization
+	void Start ()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        agent.SetDestination(target.position);
+	}
+}
